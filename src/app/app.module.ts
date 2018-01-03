9e387da16a6d0df2a97e445c3977127d2789391d
mgranberg <mgranberg89@gmail.com>
2018-01-04 00:47:20 +0100

    Added the menu and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AllNewsComponent } from './all-news/all-news.component';
+import { ExpressenComponent } from './expressen/expressen.component';
+import { SvdComponent } from './svd/svd.component';
+import { NtComponent } from './nt/nt.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllNewsComponent,
+    ExpressenComponent,
+    SvdComponent,
+    NtComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/expressen/expressen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-expressen',
+  templateUrl: './expressen.component.html',
+  styleUrls: ['./expressen.component.css']
+})
+export class ExpressenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
